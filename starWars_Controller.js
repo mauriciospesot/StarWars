93c40acb3beee9267b97627ec469cbb5137b55d5
Mauricio Spesot <mauricio.spesot@gmail.com>
2020-08-10 21:51:55 -0300

    Se agregó el metodo para poder crear un contacto

--- starWars_Controller.js
+++ starWars_Controller.js
@@ -1,32 +1,48 @@
 ({
-	handleClick : function(component, event, helper) {
-      var searchId = component.get('v.searchText');
-      var action = component.get('c.getCharacter');
-      action.setParams({searchId: searchId});
+    doInit : function(component, event, helper) {
+        console.log('update2');
+    },
+    
+    handleClick : function(component, event, helper) {
+        var searchId = component.get('v.searchText');
+        var action = component.get('c.getCharacter');
+        action.setParams({searchId: searchId});
         
-      action.setCallback(this, function(response) {
-        var state = response.getState();
-        if (state === 'SUCCESS') {
-          var characterInfo = response.getReturnValue();
-          component.set("v.characterInfo", characterInfo);
-          component.set("v.searchIsReady", true);
-        }
-      });
-
-      $A.enqueueAction(action);
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var characterInfo = response.getReturnValue();
+                component.set("v.characterInfo", characterInfo);
+                component.set("v.searchIsReady", true);
+            }
+        });
+        
+        $A.enqueueAction(action);
     },
     
     handleClickAddContact : function(component, event, helper) {
-   	 
-      var action = component.get('c.addContact');
         
-      action.setCallback(this, function(response) {
-        var state = response.getState();
-        if (state === 'SUCCESS') {
-          // handle response
-        }
-      });
-
-      $A.enqueueAction(action);
+        var action = component.get('c.addContact');
+        var info = component.get("v.characterInfo");
+        action.setParams({character: info});  
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                console.log('se creo un contacto');
+                
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Success!",
+                    "mode": 'sticky',
+                    "type": "success",
+                    "message": "The record has been created successfully."
+                });
+                toastEvent.fire();
+            } else {
+                console.log(state);
+            }
+        });
+        
+        $A.enqueueAction(action);
     }
 })
